f852745440759db70b2f359f9668b34bf8a9f402
KienTuongNguyen <kientuongnguyen27697@gmail.com>
2022-07-15 11:13:26 +0700

    Initial Commit

---
+++ SampleAtomicDesign/SampleAtomicDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleAtomicDesignApp.swift
+//  SampleAtomicDesign
+//
+//  Created by Nguyễn Kiến Tường on 15/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct SampleAtomicDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
